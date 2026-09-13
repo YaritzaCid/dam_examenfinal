280a4a5120f132a9aa3b97b47904268eba80966f
Jorge Figueroa <jorfiv28051989@gmail.com>
2026-09-13 18:12:19 -0300

    refactor: eliminar pantallas heredadas del template

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -41,13 +41,6 @@ export default function TabLayout() {
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="plus.circle.fill" color={color} />,
         }}
       />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
     </Tabs>
   );
 }
